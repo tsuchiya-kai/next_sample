9e7eaca58b2c7c053c88c437d6bfbdf1cb1d37de
tsuchiya-kai <yukaigangan@gmail.com>
2021-11-10 21:48:04 +0900

    投稿ページの編集を完了

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,5 +1,8 @@
+import Head from "next/head";
 import Layout from "/components/layout";
+import Date from "/components/date";
 import { getAllPostIds, getPostData } from "/lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
 // getStaticPaths はビルド時にどんな静的サイトを生成したのかを示す関数
 export async function getStaticPaths() {
@@ -11,7 +14,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -23,11 +26,16 @@ export default function Post({ postData }) {
   console.log({ postData });
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
